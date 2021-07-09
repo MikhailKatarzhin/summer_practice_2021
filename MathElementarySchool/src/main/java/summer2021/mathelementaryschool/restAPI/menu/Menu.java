e2aeb463381c7dfd0fcf75bb408d49fcddcd34f1
MikhailKatarzhin <maikl.1997@mail.ru>
2021-07-09 08:33:36 +0400

    Замена возврата строки на возврат класса-ответа для меню. Расширение обработки ответа в меню

--- MathElementarySchool/src/main/java/summer2021/mathelementaryschool/restAPI/menu/Menu.java
+++ MathElementarySchool/src/main/java/summer2021/mathelementaryschool/restAPI/menu/Menu.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import summer2021.mathelementaryschool.datebase.repository.ResolvedProblemRepository;
+import summer2021.mathelementaryschool.restAPI.DefaultResponse;
 import summer2021.mathelementaryschool.restAPI.authorizate.IAuthenticateJWT;
 import summer2021.mathelementaryschool.restAPI.authorizate.JWTPayload;
 
@@ -17,17 +18,17 @@ public class Menu {
     final ResolvedProblemRepository resolvedProblemRepository;
 
     @GetMapping("/menu/countRP")
-    public String getCountResolvedProblems(HttpServletRequest request){
+    public DefaultResponse getCountResolvedProblems(HttpServletRequest request){
         JWTPayload jwtPayload   = iAuthenticateJWT.checkAuthentication(request);
         if(jwtPayload == null)
-            return "jwtfail";
+            return new DefaultResponse("jwtfail", "401");
 
         Integer count           = resolvedProblemRepository.countResolvedProblemById(jwtPayload.getId_user());
         if (count == null){
-            return "error";
+            return new DefaultResponse("error", "500");
         }
 
-        return count.toString();
+        return new DefaultResponse(count.toString(), "200");
     }
 
 
--- MathElementarySchool/src/main/resources/static/js/menu.js
+++ MathElementarySchool/src/main/resources/static/js/menu.js
@@ -4,10 +4,18 @@ window.onload = function updateCountRP(){
         method: 'GET',
         contentType: "application/json",
         success: function (data) {
-            document.getElementById("countRP").innerHTML = data;
+            switch (data.key){
+                case "200":{
+                    document.getElementById("countRP").innerHTML = data.status;
+                    break;
+                }
+                default:{
+                    document.location.href = "/signin";
+                }
+            }
         },
-        error: function (data){
+        error: function (){
             document.location.href = "/signin";
         }
-    }, 60);
+    }, 600);
 }
